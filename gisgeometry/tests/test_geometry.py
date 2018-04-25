15c62bfad1b59ecd3d39e0dc9a5475f12bf6b6b3
oncox <owenncox@gmail.com>
2018-04-25 11:36:08 +0100

    Added gitignore

---
+++ gisgeometry/tests/test_geometry.py
@@ -0,0 +1,16 @@
+import unittest
+from gisgeometry import geometry
+
+class TestBasicFunction(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        self.assertTrue(True)
+
+    def test_2(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
